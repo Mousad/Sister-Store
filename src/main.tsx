fc57d6be68b3396e1b0794167b690387285b3b79
YourNewUsername <yournewemail@example.com>
2026-04-26 01:31:33 +0300

    fix build imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
 import { CartProvider } from "./app/context/CartContext";
 import "./styles/index.css";
-
-import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <CartProvider>
