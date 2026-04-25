ef403b211a506c52f87fd25140ca2f41838eb915
YourNewUsername <yournewemail@example.com>
2026-04-26 01:52:25 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
 import { CartProvider } from "./app/context/CartContext";
 import "./styles/index.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <CartProvider>
     <RouterProvider router={router} />
   </CartProvider>
